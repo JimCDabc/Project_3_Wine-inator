62f6bb8010df24a73f79d5fd8d5df8987e12e242
JimCDabc <jim.comas@gmail.com>
2020-04-01 20:34:49 -0500

    Mindmap for project and exports

---
+++ docs/Wineinator_Project_mind.mm
@@ -0,0 +1,138 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Wineinator Project" FOLDED="false" ID="ID_488007055" CREATED="1585781142383" MODIFIED="1585781344503" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Winery / Wine brewer" POSITION="right" ID="ID_1424925978" CREATED="1585781148208" MODIFIED="1585781358842">
+<edge COLOR="#ff0000"/>
+<node TEXT="Questions" ID="ID_943769497" CREATED="1585781374615" MODIFIED="1585781380170">
+<node TEXT="What chemical properties contribute to high quality wine?" ID="ID_1095157376" CREATED="1585781607057" MODIFIED="1585781628873"/>
+<node TEXT="What is the quantiy and combination of these properties that rpoduct the highest quality wines?" ID="ID_787524761" CREATED="1585781629351" MODIFIED="1585781668330"/>
+<node TEXT="What should I focus our wine producing process on to achieve high quality" ID="ID_1352293876" CREATED="1585781669007" MODIFIED="1585781699185"/>
+<node TEXT="Given the set of properties I currently can control, and the range of values I can acheive, what combination should I target to product the highest quality wine right now" ID="ID_1199840941" CREATED="1585781699631" MODIFIED="1585781803410"/>
+</node>
+<node TEXT="App Features" ID="ID_1616498032" CREATED="1585781380568" MODIFIED="1585781384057">
+<node TEXT="Data" FOLDED="true" ID="ID_325598195" CREATED="1585781812439" MODIFIED="1585781817689">
+<node TEXT="" ID="ID_841967603" CREATED="1585781821760" MODIFIED="1585781821760"/>
+</node>
+<node TEXT="Predictor" ID="ID_743571975" CREATED="1585781825415" MODIFIED="1585781828801"/>
+</node>
+<node TEXT="Presentation mechanism" ID="ID_1597075709" CREATED="1585781846175" MODIFIED="1585781858611">
+<node TEXT="For project day" FOLDED="true" ID="ID_1453969906" CREATED="1585781858616" MODIFIED="1585782028001">
+<attribute_layout NAME_WIDTH="65.24999805539852 pt" VALUE_WIDTH="65.24999805539852 pt"/>
+<attribute NAME="deliverbale" VALUE=""/>
+<node TEXT="Python Notebook?" ID="ID_1215225795" CREATED="1585781872944" MODIFIED="1585782006049">
+<attribute NAME="Confidence" VALUE=""/>
+</node>
+<node TEXT="" ID="ID_854544539" CREATED="1585782009244" MODIFIED="1585782009245">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Integrarated HTML App" ID="ID_179233315" CREATED="1585781880623" MODIFIED="1585782022625">
+<attribute NAME="Confidece" VALUE=""/>
+</node>
+<node TEXT="" ID="ID_1574068577" CREATED="1585782009241" MODIFIED="1585782009244">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="" ID="ID_1589826676" CREATED="1585782009246" MODIFIED="1585782009246"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Teammate Goals?" FOLDED="true" POSITION="left" ID="ID_926011955" CREATED="1585781182626" MODIFIED="1585781567939">
+<edge COLOR="#0000ff"/>
+<node TEXT="Graduate DABC" FOLDED="true" ID="ID_1996894346" CREATED="1585781190745" MODIFIED="1585781552418">
+<attribute NAME="Scott" VALUE="5" OBJECT="org.freeplane.features.format.FormattedNumber|5"/>
+<attribute NAME="Meaghan" VALUE="5" OBJECT="org.freeplane.features.format.FormattedNumber|5"/>
+<attribute NAME="Brittany" VALUE="5" OBJECT="org.freeplane.features.format.FormattedNumber|5"/>
+<attribute NAME="Jim" VALUE="5" OBJECT="org.freeplane.features.format.FormattedNumber|5"/>
+<node TEXT="finish HW" ID="ID_1391095811" CREATED="1585781245754" MODIFIED="1585781251067"/>
+<node TEXT="finish project" ID="ID_46099039" CREATED="1585781203545" MODIFIED="1585781207506"/>
+<node TEXT="present on wed 4/8" ID="ID_419752815" CREATED="1585781207928" MODIFIED="1585781216706"/>
+</node>
+<node TEXT="Get a Job in this field" ID="ID_583635387" CREATED="1585781222906" MODIFIED="1585781477407">
+<attribute NAME="Scott" VALUE=""/>
+<attribute NAME="Meaghan" VALUE=""/>
+<attribute NAME="Brittany" VALUE=""/>
+<attribute NAME="Jim" VALUE=""/>
+</node>
+<node TEXT="Present at Demo Do" ID="ID_1739760481" CREATED="1585781261024" MODIFIED="1585781496243">
+<attribute NAME="Scott" VALUE=""/>
+<attribute NAME="Meaghan" VALUE=""/>
+<attribute NAME="Brittany" VALUE=""/>
+<attribute NAME="Jim" VALUE=""/>
+</node>
+<node TEXT="Get your life back" ID="ID_898112614" CREATED="1585781531114" MODIFIED="1585781604068">
+<attribute NAME="Scott" VALUE=""/>
+<attribute NAME="Meaghan" VALUE=""/>
+<attribute NAME="Brittany" VALUE=""/>
+<attribute NAME="Jim" VALUE=""/>
+</node>
+</node>
+<node TEXT="Missouri Wine Tourist" POSITION="right" ID="ID_25089717" CREATED="1585781361065" MODIFIED="1585781371698">
+<edge COLOR="#00ff00"/>
+<node TEXT="Questions" ID="ID_406828421" CREATED="1585781397815" MODIFIED="1585781843129"/>
+<node TEXT="App Features" ID="ID_1921647725" CREATED="1585781401583" MODIFIED="1585781408122"/>
+</node>
+</node>
+</map>
